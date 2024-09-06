f77f994776a98a19c9f856fdcd1de54fee5c45b6
AM-PC-46 <bhoitepushpak6@gmail.com>
2024-09-06 10:25:13 +0530

    hello s

--- React/Scrimaba/04-todo-list/src/components/List.jsx
+++ React/Scrimaba/04-todo-list/src/components/List.jsx
@@ -4,11 +4,11 @@ export default function List(props) {
   return (
     <li >
       <div className='row '>
-        <div className='col-6'>
-          <span >{props.dataVal}</span>
+        <div className='col-6 '>
+          {props.editing ? <input type='text' className='form-control' onChange={(event)=> props.onEditing(event,props.id)} value={props.dataVal} name='tName'/> : <span >{props.dataVal}</span>}
         </div>
         <div className='col-6 mb-3'>
-          <button className='d-inline-flex  btn btn-warning' onClick={props.editTask}>Edit</button>
+          <button className='d-inline-flex  btn btn-warning' onClick={props.editTask}>{props.editing ?'Save':'Edit'}</button>
         </div>
       </div>
     </li>
--- React/Scrimaba/04-todo-list/src/components/Section.jsx
+++ React/Scrimaba/04-todo-list/src/components/Section.jsx
@@ -1,47 +1,65 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import List from './List'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 
 export default function () {
 
-const [arrData, setArrData ] = useState(()=>{
-    return JSON.parse(localStorage.getItem('storage'))|| []
-}
-)
-const [formData, setFormData] = useState({
-    id: nanoid(),
-    tName:'',
-    editing: false
-});
+    const [arrData, setArrData] = useState(() => {
+        return JSON.parse(localStorage.getItem('storage')) || []
+    }
+    )
+    const [formData, setFormData] = useState({
+        id: nanoid(),
+        tName: '',
+        editing: false
+    });
 
-function handleChange(event){
-     let {name,type,checked,value }= event.target  
-    setFormData((prev)=> {
-        return {...prev, [name]:value}
-})
-}
-useEffect(()=>{
+    function handleChange(event) {
+        let { name, type, checked, value } = event.target
+        setFormData((prev) => {
+            return { ...prev, [name]: value }
+        })
+    }
 
-    localStorage.setItem('storage',JSON.stringify(arrData));
+    useEffect(() => {
+        localStorage.setItem('storage', JSON.stringify(arrData));
+    }, [arrData])
 
-},[arrData])
+    useEffect(() => {
+        setArrData(prev => {
+            //   console.log('prev',prev);
+            let x = prev?.map((data) => {
+                console.log('data',{ ...data, editing: false })
+                return { ...data, editing: false }
+            })
+              console.log('X',x);
 
-function onSave(){
-        setArrData(prev => [...prev,formData])
+            return x
+        });
+    }, [])
+
+    function onSave() {
+        setArrData(prev => [...prev, formData])
         setFormData({
-            tName:'',
-            id:null,
+            tName: '',
+            id: null,
             editing: false
         });
-        
-    }
-    console.log('arrData',arrData)
 
-    function editTask(id){
-       setArrData((prev)=> prev?.map((e)=> id === e.id ? {...e,editing:!e.editing}: e ))
     }
-    // console.log('formData',formData)
+    console.log('arrData', arrData)
+
+    function editTask(id) {
+        setArrData((prev) => prev?.map((e) => id === e.id ? { ...e, editing: !e.editing } : e))
+    }
+
+    function onEditing(event, id){
+        console.log(event.target.value, id)
+        let {name, value} = event.target;
+        setArrData( prev => prev?.map(e => id === e.id ? {...e,[name]:value} :  e  ))
+    }
+
     return (
         <div>
 
@@ -58,7 +76,7 @@ function onSave(){
                             <div id="toDo" className="my-4">
                                 <form action="">
                                     <div className="todo-box  input-group rounded-pill">
-                                        <input onChange={handleChange} value={formData.tName} name="tName"type="text" placeholder="Task" className="form-control" id="todoInput" />
+                                        <input onChange={handleChange} value={formData.tName} name="tName" type="text" placeholder="Task" className="form-control" id="todoInput" />
                                         <button className="btn " onClick={onSave} type="button" id="addBtn">Add Task</button>
                                         <button className="btn " onClick={onSave} type="button" id="saveBtn" name="">Save</button>
                                     </div>
@@ -69,7 +87,7 @@ function onSave(){
                                 <div className="ul-outer">
                                     <ul id="ulShow">
                                         {
-                                            arrData?.map((data)=> <List key={data.id} id={data.id} editTask={()=>editTask(data.id)} dataVal={data.tName} editing={data.editing} />)
+                                            arrData?.map((data) => <List key={data.id} id={data.id} editTask={() => editTask(data.id)} handleChange={handleChange} dataVal={data.tName} editing={data.editing} onEditing={onEditing}/>)
                                         }
                                     </ul>
                                 </div>
@@ -83,9 +101,6 @@ function onSave(){
                             <p>Angular Minds</p>
                         </div>
                     </div>
-
-
-
 
                 </div>
             </div>
